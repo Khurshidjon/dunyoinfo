97e8a7db5683b7575d6cff3ad7d6e4fd2b75f2c7
Khurshidjon <mr.xurshidjon1995@gmail.com>
2019-11-07 15:40:21 +0500

    ddd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -229,7 +229,7 @@
 					<div class="container">
 						<nav id="mainmenu" class="navbar-left collapse navbar-collapse">
 							<ul class="nav navbar-nav">
-								<li class="home"><a href="{{ route('frontend.index') }}" class="">Home</a></li>
+								<li class="home"><a href="{{ route('frontend.index') }}" class="">@lang('pages.home_menu')</a></li>
 								@foreach ($menus as $menu)
 								@php
 									//dd($menu->hasParent())
@@ -245,284 +245,6 @@
 										@endif
 									</li>
 								@endforeach
-								{{-- <li class="politics dropdown"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Politics</a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Dropdown Demo</a></li>
-										<li><a href="#">Dropdown Demo</a></li>
-										<li><a href="#">Dropdown Demo</a></li>
-										<li><a href="#">Dropdown Demo</a></li>
-									</ul>
-								</li>
-								<li class="environment dropdown mega-dropdown"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Environment</a>
-									<div class="dropdown-menu mega-menu">
-										<div class="container">
-											<div class="row">
-												<div class="col-sm-3">
-													<h2>Template Features</h2>
-													<ul>
-														<li><a href="#">Multi Home Variations</a></li>
-														<li><a href="#">Multi Color Menu</a></li>
-														<li><a href="#">Fully Responsive</a></li>
-														<li><a href="#">Mega Dropdown</a></li>
-														<li><a href="#">Creative Design</a></li>
-													</ul>
-												</div>
-												<div class="col-sm-3">
-													<h2>Included Pages</h2>
-													<ul>
-														<li><a href="{{ route('frontend.single-news') }}">News Details</a></li>
-														<li><a href="listing.html">Category Listing</a></li>
-														<li><a href="listing-life-style.html">Category Lifestyle</a></li>
-														<li><a href="listing-sports.html">Category Sports</a></li>
-														<li><a href="404.html">404 Error</a></li>
-													</ul>
-												</div>
-												<div class="col-sm-3">
-													<h2>Menu with image</h2>
-													<div class="entry-thumbnail">
-														<img class="img-responsive" src="{{ asset('frontend/images/post/4.jpg') }}" alt="" />
-													</div>
-												</div>
-												<div class="col-sm-3">
-													<h2>Menu with video</h2>
-													<div class="entry-thumbnail embed-responsive embed-responsive-16by9">
-														<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/-WlqrkXImsk" allowfullscreen></iframe>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</li>
-								<li class="world dropdown mega-cat-dropdown">
-									<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">World</a>
-									<div class="dropdown-menu mega-cat-menu">
-										<div class="container">
-											<div class="sub-catagory">
-												<h2 class="section-title">World</h2>
-												<ul class="list-inline">
-													<li><a href="#">Usa</a></li>
-													<li><a href="#">Asia</a></li>
-													<li><a href="#">Us</a></li>
-													<li><a href="#">Politics</a></li>
-													<li><a href="#">Business</a></li>
-													<li><a href="#">Press</a></li>
-													<li><a href="#">Travel</a></li>
-												</ul>
-											</div>
-											<div class="row">
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/2.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Apple may be preparing for new Beats radio stations</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/6.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Why is the media so afraid of Facebook?</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/3.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Samsung Pay will support online shopping</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/5.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">The best games for your phone</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-											</div>
-										</div>
-									</div>
-								</li>
-								<li class="technology dropdown mega-cat-dropdown">
-									<a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Technology</a>
-									<div class="dropdown-menu mega-cat-menu">
-										<div class="container">
-											<div class="sub-catagory">
-												<h2 class="section-title">Technology</h2>
-												<ul class="list-inline">
-													<li><a href="#">Apps</a></li>
-													<li><a href="#">Gear</a></li>
-													<li><a href="#">Watch</a></li>
-													<li><a href="#">Tech</a></li>
-													<li><a href="#">Laptop</a></li>
-													<li><a href="#">Mobile</a></li>
-													<li><a href="#">3D Games</a></li>
-												</ul>
-											</div>
-											<div class="row">
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/technology/6.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Apple may be preparing for new Beats radio stations</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/technology/5.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Why is the media so afraid of Facebook?</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/technology/4.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">Samsung Pay will support online shopping</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-												<div class="col-sm-3">
-													<div class="post medium-post">
-														<div class="entry-header">
-															<div class="entry-thumbnail">
-																<img class="img-responsive" src="{{ asset('frontend/images/post/technology/3.jpg') }}" alt="" />
-															</div>
-														</div>
-														<div class="post-content">
-															<div class="entry-meta">
-																<ul class="list-inline">
-																	<li class="publish-date"><a href="#"><i class="fa fa-clock-o"></i> Nov 5, 2015 </a></li>
-																	<li class="views"><a href="#"><i class="fa fa-eye"></i>15k</a></li>
-																	<li class="loves"><a href="#"><i class="fa fa-heart-o"></i>278</a></li>
-																</ul>
-															</div>
-															<h2 class="entry-title">
-																<a href="{{ route('frontend.single-news') }}">The best games for your phone</a>
-															</h2>
-														</div>
-													</div><!--/post-->
-												</div>
-											</div>
-										</div>
-									</div>
-								</li>
-								<li class="sports"><a href="{{ route('frontend.listing-sports') }}">Sports</a></li>
-								<li class="health"><a href="{{ route('frontend.health') }}">Health</a></li>
-								<li class="entertainment"><a href="{{ route('frontend.entertainment') }}">Entertainment</a></li>
-								<li class="lifestyle"><a href="{{ route('frontend.lifestyle') }}">Lifestyle</a></li>
-								<li class="more dropdown"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">Pages +</a>
-									<ul class="dropdown-menu">
-										<li><a href="{{ route('frontend.single-news') }}">News Details</a></li>
-										<li><a href="listing.html">Category Listing</a></li>
-										<li><a href="listing-life-style.html">Category Lifestyle</a></li>
-										<li><a href="listing-sports.html">Category Sports</a></li>
-										<li><a href="404.html">404 Error</a></li>
-										<li><a href="author-listing.html">Author Listing <span class="badge">New</span></a></li>
-										<li><a href="author-details.html">Author Details <span class="badge">New</span></a></li>
-										<li><a href="about-us.html">About Us <span class="badge">New</span></a></li>
-										<li><a href="contact-us.html">Contact Us <span class="badge">New</span></a></li>
-										<li><a href="contact-us2.html">Contact Us Two <span class="badge">New</span></a></li>
-										<li><a href="signup.html">Sign Up Page<span class="badge">New</span></a></li>
-										<li><a href="signin.html">Sign In page<span class="badge">New</span></a></li>
-									</ul>
-								</li> --}}
 							</ul>
 						</nav>
 					</div>
@@ -690,20 +412,20 @@
 
 
 		<!--/Preset Style Chooser-->
-	<div class="style-chooser">
-		<div class="style-chooser-inner">
-			<a href="#" class="toggler"><i class="fa fa-life-ring fa-spin"></i></a>
-			<h4>Presets Color</h4>
-			<ul class="preset-list clearfix">
-				<li class="preset1 active" data-preset="1"><a href="#" data-color="preset1"></a></li>
-				<li class="preset2" data-preset="2"><a href="#" data-color="preset2"></a></li>
-				<li class="preset3" data-preset="3"><a href="#" data-color="preset3"></a></li>
-				<li class="preset4" data-preset="4"><a href="#" data-color="preset4"></a></li>
-				<li class="preset5" data-preset="5"><a href="#" data-color="preset5"></a></li>
-				<li class="preset6" data-preset="6"><a href="#" data-color="preset6"></a></li>
-			</ul>
-		</div>
-    </div>
+{{--	<div class="style-chooser">--}}
+{{--		<div class="style-chooser-inner">--}}
+{{--			<a href="#" class="toggler"><i class="fa fa-life-ring fa-spin"></i></a>--}}
+{{--			<h4>Presets Color</h4>--}}
+{{--			<ul class="preset-list clearfix">--}}
+{{--				<li class="preset1 active" data-preset="1"><a href="#" data-color="preset1"></a></li>--}}
+{{--				<li class="preset2" data-preset="2"><a href="#" data-color="preset2"></a></li>--}}
+{{--				<li class="preset3" data-preset="3"><a href="#" data-color="preset3"></a></li>--}}
+{{--				<li class="preset4" data-preset="4"><a href="#" data-color="preset4"></a></li>--}}
+{{--				<li class="preset5" data-preset="5"><a href="#" data-color="preset5"></a></li>--}}
+{{--				<li class="preset6" data-preset="6"><a href="#" data-color="preset6"></a></li>--}}
+{{--			</ul>--}}
+{{--		</div>--}}
+{{--    </div>--}}
 	<!--/End:Preset Style Chooser-->
 
 	<!--/#scripts-->
